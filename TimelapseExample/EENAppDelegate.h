77d9d1b3a39acc7094fefd3422affd1ad6f0c81c
cazares <miguel.cazares89@gmail.com>
2014-05-27 13:06:09 -0500

    Initial Commit

---
+++ TimelapseExample/EENAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EENAppDelegate.h
+//  TimelapseExample
+//
+//  Created by Miguel Cazares on 5/27/14.
+//  Copyright (c) 2014 EagleEye Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EENAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimelapseExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimelapseExample
+//
+//  Created by Miguel Cazares on 5/27/14.
+//  Copyright (c) 2014 EagleEye Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EENAppDelegate class]));
+    }
+}
